df3f7bf22d7d11d33f4c81f87c28095886c6a7a8
OrNahum <ornahum72@gmail.com>
2024-11-19 20:52:39 +0200

    s3sync

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "davshe-technion-cicd-13579"
+    bucket         = "just-a-simple-s3-bucket-testing"
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
